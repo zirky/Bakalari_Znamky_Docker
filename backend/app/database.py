7b72d23e268664f192fe8d42692bdbf70cbb2e25
zirky <90569126+zirky@users.noreply.github.com>
2026-08-22 23:23:16 +0200

    Update DATABASE_URL to new database path

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, Session, declarative_base
 from datetime import datetime
 from typing import Generator
 
-DATABASE_URL = "sqlite:////app.db"
+DATABASE_URL = "sqlite:////data/app.db"
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}, echo=False
