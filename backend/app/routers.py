f86f0126c57dcd94051aa79d24ce68a218a73f0f
zirky <90569126+zirky@users.noreply.github.com>
2026-09-20 21:28:32 +0200

    Fix indentation for normalized_grade assignment

--- backend/app/routers.py
+++ backend/app/routers.py
@@ -1264,7 +1264,7 @@ def child_overview(
             'description': grade.description,
         })
 
-                normalized_grade = absolute_grade_value(
+        normalized_grade = absolute_grade_value(
             grade.grade_value
         )
 
@@ -1273,7 +1273,7 @@ def child_overview(
                 grade.subject,
                 [],
             ).append(int(normalized_grade))
-    
+
     subjects = [
         {
             'subject': subject,
